3049ecba36463c574326efc7e8a7ed5d1217dae9
ryan508 <ryan.p.curtin@gmail.com>
2013-04-09 12:04:59 -0400

    Market data using node/redis

--- market_data.js
+++ market_data.js
@@ -1,8 +1,14 @@
-// var io = require('socket.io-client');
-// var feed = io.connect('https://feed.bitfloor.com/1');
+// Setup connection to real-time market data
+var io = require('socket.io-client');
+var feed = io.connect('https://feed.bitfloor.com/1');
 
-// feed.on('book', function(book) {
-//   console.log(book);
-// });
+// Using redis client as datastore
+var redis = require('redis');
+var client = redis.createClient();
 
-// feed.emit('book');
+// Logging completed orders in Redis
+feed.on('match', function(data) {
+  console.log('seq', data['seq'])
+  client.sadd('trades', data['seq'])
+  client.hmset(data['seq'], 'timestamp', data['timestamp'], 'price', data['price'], 'size', data['size'])
+});
