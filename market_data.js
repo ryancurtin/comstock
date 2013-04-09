172324aea81ade473b139ad668b16c20156a814c
ryan508 <ryan.p.curtin@gmail.com>
2013-04-09 01:31:27 -0400

    Realtime data - basic connection working with node

---
+++ market_data.js
@@ -0,0 +1,8 @@
+// var io = require('socket.io-client');
+// var feed = io.connect('https://feed.bitfloor.com/1');
+
+// feed.on('book', function(book) {
+//   console.log(book);
+// });
+
+// feed.emit('book');
